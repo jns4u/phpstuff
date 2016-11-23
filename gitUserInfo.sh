eb4a0539f68cf594596168b0ec728a615da7d196
Jai Narayan Singh <jainarayan1023@gmail.com>
2016-11-24 03:37:08 +0530

    Create gitUserInfo.sh
    this script fetches gitUser info

---
+++ gitUserInfo.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+ echo "Usage: $0 <username>"
+ exit 1
+fi
+
+curl -s "https://api.github.com/users/$1" | \
+ awk -F'"' '
+ /\"name\":/ {
+  print $4" is the name of the GitHub user."
+ }
+ /\"followers\":/{
+  split($3, a, " ")
+  sub(/,/, "", a[2])
+  print "They have "a[2]" followers."
+ }
+ /\"following\":/{
+  split($3, a, " ")
+  sub(/,/, "", a[2])
+  print "They are following "a[2]" other users."
+ }
+ /\"created_at\":/{
+  print "Their account was created on "$4"."
+ }
+ '
+exit 0
