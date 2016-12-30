cab06b808af7cf91073ae22d18926d3921e1de49
Jai Narayan Singh <jainarayan1023@gmail.com>
2016-12-31 01:25:03 +0530

    updated file
    added source encoding

--- webscrapping.php
+++ webscrapping.php
@@ -1,6 +1,18 @@
 <?php
-
-//CURL to interchange Info between two Servers	
+/** source function for encoding
+function utf8ize($dat) {
+    if (is_array($dat)) {
+        foreach ($dat as $key => $val) {
+            $dat[$key] = utf8ize($val);
+        }
+    } else if (is_string ($dat)) {
+        return utf8_encode($dat);
+    }
+    return $dat;
+}
+ 
+CURL to interchange Info between two Servers	
+*/
 $ch = curl_init();			
 curl_setopt($ch, CURLOPT_URL,"http://");			        //type intended url here
 curl_setopt($ch, CURLOPT_POST, true);			
